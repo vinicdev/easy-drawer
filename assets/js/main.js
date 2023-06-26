bb2c24c87b916bb92a8e6640097f8480efaf8bb5
Vinicius Cardoso <vinic8670@gmail.com>
2023-06-26 20:29:34 -0300

    changing the visibility rule of the container

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,26 @@
 let result = [];
 let numberInitial = 0;
 let numberFinal = 0;
+let incrementNumber = 0;
 let resultText = document.querySelector('#result');
 let previousResult = document.querySelector('#previousResult');
 
 function getNumber() {
     numberFinal = document.querySelector('#finalNumber').value; 
-
+    
     $('.container__initial').addClass('hide');
 
+    if(incrementNumber === 0) {
+        removeContainer();
+    }
+
     getRandom(numberFinal);
+}
+
+function removeContainer() {
+    $('.container__box').addClass('hide');
+    $('.raffleBtn').addClass('show');
+    incrementNumber++;
 }
 
 function getRandom(max) {
@@ -37,8 +48,6 @@ function verifyNumber(testNumber) {
         result.push(testNumber);
         getPrint(testNumber);
     }
-
-   
 }
 
 function getPrint(valueFinal) {
